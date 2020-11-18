5f5681665043206e1a95f3217fb30d339761e3df
Gianpiero <gianpiero.costanza@rakuten.com>
2020-11-18 22:09:42 +0100

    get products

--- gatsby-config.js
+++ gatsby-config.js
@@ -29,7 +29,8 @@ module.exports = {
       options: {
         shopName: process.env.SHOPIFY_SHOP_NAME,
         accessToken: process.env.SHOPIFY_TOKEN,
-        // includeCollections: ["shop"],
+        includeCollections: ["shop", "content"],
+        verbose: true,
       },
     },
     {
@@ -41,7 +42,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/gatsby-icon.png`,
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -1,20 +1,46 @@
 import React from "react"
-import { Link } from "gatsby"
+import { Link, graphql } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-import List from "../components/List"
-import Header from "../components/header"
 
-const SecondPage = () => (
+export const query = graphql`
+  {
+    allShopifyProduct(sort: { fields: [title] }) {
+      edges {
+        node {
+          title
+          shopifyId
+          description
+          handle
+          priceRange {
+            minVariantPrice {
+              amount
+            }
+          }
+        }
+      }
+    }
+  }
+`
+
+const SecondPage = ({ data }) => (
   <Layout>
     <SEO title="Page two" />
-    <Header />
-    <List />
-    <h1>Hi from the second page</h1>
-    <p>Welcome to page 2</p>
+    <h1>Products</h1>
+    <ul>
+      {data.allShopifyProduct.edges.map(({ node }) => (
+        <li key={node.shopifyId}>
+          <h3>
+            <Link to={`/product/${node.handle}`}>{node.title}</Link>
+            {" - "}${node.priceRange.minVariantPrice.amount}
+          </h3>
+          <p>{node.description}</p>
+        </li>
+      ))}
+    </ul>
+
     <Link to="/">Go back to the homepage</Link>
   </Layout>
 )
-
 export default SecondPage
